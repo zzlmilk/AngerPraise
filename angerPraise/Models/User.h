5bf4955dbdd14ff9840aaa1d7d7b9b01d6e8d5fe
zhilingzhou <zhilingzhou@zhilingzhoudeMacBook-Pro.local>
2015-03-09 14:57:43 +0800

    m

---
+++ angerPraise/Models/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  angerPraise
+//
+//  Created by zhilingzhou on 15/3/9.
+//  Copyright (c) 2015年 Rex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
