341ec0cd71d33b8a1aff0b3564da7d74fee99b28
xbruence <372260652@qq.com>
2015-04-09 19:04:38 +0800

    虚拟投递对接成功

---
+++ angerPraise/Models/Resume.h
@@ -0,0 +1,13 @@
+//
+//  Resume.h
+//  angerPraise
+//
+//  Created by 单好坤 on 15/4/9.
+//  Copyright (c) 2015年 Rex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Resume : NSObject
+
+@end
---
+++ angerPraise/Models/Resume.m
@@ -0,0 +1,13 @@
+//
+//  Resume.m
+//  angerPraise
+//
+//  Created by 单好坤 on 15/4/9.
+//  Copyright (c) 2015年 Rex. All rights reserved.
+//
+
+#import "Resume.h"
+
+@implementation Resume
+
+@end
