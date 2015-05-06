5f25945b4cda6e4113b3bb4431449bb0dd89590e
xbruence <372260652@qq.com>
2015-05-06 11:23:03 +0800

    修改引用文件

---
+++ angerPraise/Models/InterviewPay.h
@@ -0,0 +1,13 @@
+//
+//  InterviewPay.h
+//  angerPraise
+//
+//  Created by 单好坤 on 15/5/6.
+//  Copyright (c) 2015年 Rex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InterviewPay : NSObject
+
+@end
---
+++ angerPraise/Models/InterviewPay.m
@@ -0,0 +1,13 @@
+//
+//  InterviewPay.m
+//  angerPraise
+//
+//  Created by 单好坤 on 15/5/6.
+//  Copyright (c) 2015年 Rex. All rights reserved.
+//
+
+#import "InterviewPay.h"
+
+@implementation InterviewPay
+
+@end
