5e7be9ef4ddfacf87bb14cfbbd0bad22775df8ab
xbruence <372260652@qq.com>
2015-06-05 19:53:24 +0800

    as

---
+++ angerPraise/Models/Setting.h
@@ -0,0 +1,13 @@
+//
+//  Setting.h
+//  angerPraise
+//
+//  Created by 单好坤 on 15/6/4.
+//  Copyright (c) 2015年 Rex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Setting : NSObject
+
+@end
